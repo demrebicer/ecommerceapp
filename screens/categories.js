a60bdfcba5c88890b8d3b703680e0e34b252b9b3
aybars <aybars.gocerler@bahcesehir.edu.tr>
2021-05-29 18:35:50 +0300

    fixing corrupted data bcs of back-end

--- screens/categories.js
+++ screens/categories.js
@@ -12,14 +12,13 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import axios from 'axios';
 import { ListItem, Avatar } from 'react-native-elements';
-import { Appbar,List,Colors,Divider,IconButton } from 'react-native-paper';
+import { Appbar, List, Colors, Divider, IconButton } from 'react-native-paper';
 import { StyleSheet } from 'react-native';
 import HomeScreen from '../App';
 import { useNavigation } from '@react-navigation/native';
 
-
 function CategoriesPage({ navigation }) {
-  const nav = useNavigation(); 
+  const nav = useNavigation();
   const [data, setdata] = useState([]);
   function deleteCategory(id) {
     const newList = data.filter((data) => data.id !== id);
@@ -28,37 +27,68 @@ function CategoriesPage({ navigation }) {
   }
 
   useEffect(() => {
+    if (data.length == 0) {
       axios
         .get('https://northwind.vercel.app/api/categories')
         .then((response) => {
-          setdata(response.data);
-          console.log(response.data, 'aaaa');
+          let dt = [];
+          for (let i = 0; i < response.data.length; i++) {
+            try {
+              temp = {};
+              temp.id = response.data[i].id;
+              temp.name = response.data[i].name;
+              temp.description = response.data[i].description;
+              if (temp.name == undefined) {
+                continue;
+              }
+              if (temp.description == undefined) {
+                continue;
+              }
+              dt.push(temp);
+            } catch (Exc) {
+              console.log('hatalı');
+              continue;
+            }
+          }
+          setdata(dt);
         })
         .catch((error) => {
           console.log(error);
         });
+    }
   });
 
   keyExtractor = (item, index) => index.toString();
 
   renderItem = ({ item }) => (
-        <List.Item
-      title={item.name.value == null ? item.name : item.name.value} 
+    <List.Item
+      title={item.name.value == null ? item.name : item.name.value}
       // description="Item description"
       description={item.description}
-      left={props => <List.Icon color={Colors.grey600} size={28} icon="folder" />}
-      right={props =>   <IconButton
-                          icon="delete"
-                          color={Colors.red700}
-                          size={28}
-                          onPress={() => {
-                            console.log(item.id)
-                            deleteCategory(item.id)}}
-                        />
-                        }
+      left={(props) => (
+        <List.Icon color={Colors.grey600} size={28} icon="folder" />
+      )}
+      right={(props) => (
+        <IconButton
+          icon="delete"
+          color={Colors.red700}
+          size={28}
+          onPress={() => {
+            console.log(item.id);
+            deleteCategory(item.id);
+          }}
+        />
+      )}
       style={{
         borderBottomColor: 'grey',
         borderBottomWidth: 0.3,
+      }}
+      onPress={() => {
+        nav.navigate('updateCategoryPage', {
+          name: item.name.value == null ? item.name : item.name.value,
+          desc: item.description,
+          id: item.id,
+        });
       }}
     />
   );
@@ -68,16 +98,18 @@ function CategoriesPage({ navigation }) {
       <Appbar.Header>
         {/* <Appbar.BackAction onPress={_goBack} /> */}
         <Appbar.Content title="Categories" />
-        <Appbar.Action icon="plus-box"  onPress={() => nav.navigate('addCategoryPage')}/>
+        <Appbar.Action
+          icon="plus-box"
+          onPress={() => nav.navigate('addCategoryPage')}
+        />
       </Appbar.Header>
 
-
-      <FlatList style={{marginBottom:100}}
+      <FlatList
+        style={{ marginBottom: 100 }}
         keyExtractor={this.keyExtractor}
         data={data}
         renderItem={this.renderItem}
       />
-
     </View>
   );
 }
